e512a693b4613c06330bfda763d396b37583582d
jitrogith <coel98er@gmail.com>
2020-02-14 00:39:26 +0700

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I've been Changed on master!
+          Welcome Jitero san!
         </p>
         <a
           className="App-link"
