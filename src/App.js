83d7fae108d15cd86e616bd692a44a56ffed7048
jitrogith <coel98er@gmail.com>
2020-02-14 08:24:12 +0700

    change from feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I've been Changed !
+          I've been Changed from feature !
         </p>
         <a
           className="App-link"
