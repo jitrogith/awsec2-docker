3be2ac3ebec535e132c7e38a6d3e74b5f51b9ec0
jitrogith <coel98er@gmail.com>
2020-02-14 09:02:41 +0700

    change fr feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I've been Changed from feature !
+          GANTI DARI FEATURE !
         </p>
         <a
           className="App-link"
