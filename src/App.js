18bd64bf64beced818f11cc9b465edadd53c998a
jitrogith <coel98er@gmail.com>
2020-02-14 08:36:04 +0700

    change from cabang1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome Jitero san!
+          Changed from cabang1!
         </p>
         <a
           className="App-link"
